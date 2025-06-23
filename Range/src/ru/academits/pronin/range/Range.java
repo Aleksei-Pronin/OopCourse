2975f87c7233d8b3e4b3c26f5314388a0372f0a2
Pronin Aleksei <aleksei.s.pronin@gmail.com>
2025-06-24 00:21:14 +0700

    Add Range project

--- Range/src/ru/academits/pronin/range/Range.java
+++ Range/src/ru/academits/pronin/range/Range.java
@@ -1,4 +1,33 @@
 package ru.academits.pronin.range;
 
 public class Range {
+    private double from;
+    private double to;
+
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public boolean isInside(double number) {
+        return number >= from && number <= to;
+    }
+
+
 }
--- Range/src/ru/academits/pronin/range_main/Main.java
+++ Range/src/ru/academits/pronin/range_main/Main.java
@@ -1,4 +1,13 @@
 package ru.academits.pronin.range_main;
 
+import ru.academits.pronin.range.Range;
+
 public class Main {
+    public static void main(String[] args) {
+        Range range = new Range(1.1, 10.9);
+
+        double number = 5.0;
+
+        System.out.println(range.isInside(number));
+    }
 }
