699b1e8899084a16b3c7bf742a930f04f54be354
Pronin Aleksei <aleksei.s.pronin@gmail.com>
2025-06-24 21:29:01 +0700

    Final commit

--- Range/src/ru/academits/pronin/range/Range.java
+++ Range/src/ru/academits/pronin/range/Range.java
@@ -25,9 +25,50 @@ public class Range {
         this.to = to;
     }
 
+    public double getRangeLength() {
+        return this.to - this.from;
+    }
+
     public boolean isInside(double number) {
         return number >= from && number <= to;
     }
 
+    public Range getIntersection(Range range) {
+        if (this.getTo() <= range.getFrom()) {
+            return null;
+        }
 
+        return new Range(range.getFrom(), this.getTo());
+    }
+
+    public Range[] getUnion(Range range) {
+        if (this.getTo() >= range.getFrom()) {
+            return new Range[]{new Range(this.getFrom(), range.getTo())};
+        }
+
+        return new Range[]{this, range};
+    }
+
+    public Range[] getDifference(Range range) {
+        if (this.getFrom() >= range.getFrom() && this.getTo() <= range.getTo()) {
+            return new Range[0];
+        }
+
+        if (this.getFrom() < range.getFrom() && this.getTo() > range.getTo()) {
+            return new Range[]{new Range(this.getFrom(), range.getFrom()), new Range(range.getTo(), this.getTo())};
+        }
+
+        if (this.getFrom() >= range.getTo() || this.getTo() <= range.getFrom()) {
+            return new Range[]{this};
+        }
+
+        return (this.getFrom() < range.getFrom()) ?
+                new Range[]{new Range(this.getFrom(), range.getFrom())} :
+                new Range[]{new Range(range.getTo(), this.getTo())};
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getFrom() + ", " + this.getTo() + "]";
+    }
 }
--- Range/src/ru/academits/pronin/range_main/Main.java
+++ Range/src/ru/academits/pronin/range_main/Main.java
@@ -2,12 +2,38 @@ package ru.academits.pronin.range_main;
 
 import ru.academits.pronin.range.Range;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Range range = new Range(1.1, 10.9);
+        Scanner scanner = new Scanner(System.in);
 
-        double number = 5.0;
+        System.out.println("Введите начало интервала 1:");
+        double from1 = scanner.nextDouble();
 
-        System.out.println(range.isInside(number));
+        System.out.println("Введите конец интервала 1:");
+        double to1 = scanner.nextDouble();
+
+        Range range1 = new Range(from1, to1);
+
+        System.out.println("Длина интервала " + range1 + " - " + range1.getRangeLength());
+
+        System.out.println("Введите число:");
+        double number = scanner.nextDouble();
+
+        System.out.println(range1.isInside(number) ? "Число " + number + " принадлежит интервалу " + range1 : "Число " + number + " не принадлежит интервалу " + range1);
+
+        System.out.println("Введите начало интервала 2:");
+        double from2 = scanner.nextDouble();
+
+        System.out.println("Введите конец интервала 2:");
+        double to2 = scanner.nextDouble();
+
+        Range range2 = new Range(from2, to2);
+
+        System.out.println("Пересечение интервалов " + range1 + " и " + range2 + " - " + range1.getIntersection(range2));
+        System.out.println("Объединение интервалов " + range1 + " и " + range2 + " - " + Arrays.toString(range1.getUnion(range2)));
+        System.out.println("Разность интервалов " + range1 + " и " + range2 + " - " + Arrays.toString(range1.getDifference(range2)));
     }
 }
