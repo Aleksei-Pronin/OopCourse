69b078d754d5efb7266c84439516e48973b64cc4
Pronin Aleksei <aleksei.s.pronin@gmail.com>
2025-06-25 22:34:05 +0700

    Add different methods

--- Vector/src/ru/academits/pronin/vector/Vector.java
+++ Vector/src/ru/academits/pronin/vector/Vector.java
@@ -35,6 +35,120 @@ public class Vector {
 
     @Override
     public String toString() {
-        return Arrays.toString(coordinates);
+        int lastCoordinateIndex = this.getSize() - 1;
+
+        StringBuilder stringBuilder = new StringBuilder("{");
+
+        for (int i = 0; i < lastCoordinateIndex; i++) {
+            stringBuilder.append(coordinates[i]).append(", ");
+        }
+
+        stringBuilder.append(coordinates[lastCoordinateIndex]).append("}");
+
+        return stringBuilder.toString();
+    }
+
+    public void addVector(Vector vector) {
+        int thisSize = this.getSize();
+        int vectorSize = vector.getSize();
+
+        if (thisSize < vectorSize) {
+            this.coordinates = Arrays.copyOf(coordinates, vectorSize);
+        }
+
+        for (int i = 0; i < vectorSize; i++) {
+            this.coordinates[i] += vector.coordinates[i];
+        }
+    }
+
+    public void subtractVector(Vector vector) {
+        int thisSize = this.getSize();
+        int vectorSize = vector.getSize();
+
+        if (thisSize < vectorSize) {
+            this.coordinates = Arrays.copyOf(coordinates, vectorSize);
+        }
+
+        for (int i = 0; i < vectorSize; i++) {
+            this.coordinates[i] -= vector.coordinates[i];
+        }
+    }
+
+    public void scalarMultiplication(double scalar) {
+        int size = this.getSize();
+
+        for (int i = 0; i < size; i++) {
+            coordinates[i] *= scalar;
+        }
+    }
+
+    public void reverseVector() {
+        this.scalarMultiplication(-1.0);
+    }
+
+    public double getVectorLength() {
+        double lengthSqr = 0;
+
+        for (double coordinate : coordinates) {
+            lengthSqr += Math.pow(coordinate, 2);
+        }
+
+        return Math.sqrt(lengthSqr);
+    }
+
+    public void setCoordinate(double coordinate, int index) {
+        coordinates[index] = coordinate;
+    }
+
+    public double getCoordinate(int index) {
+        return coordinates[index];
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Vector vector = (Vector) o;
+
+        return getSize() == vector.getSize() && Arrays.equals(coordinates, vector.coordinates);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(coordinates);
+    }
+
+    public static Vector getSum(Vector vector1, Vector vector2) {
+        Vector result = new Vector(vector1);
+
+        result.addVector(vector2);
+
+        return result;
+    }
+
+    public static Vector getDifference(Vector vector1, Vector vector2) {
+        Vector result = new Vector(vector1);
+
+        result.subtractVector(vector2);
+
+        return result;
+    }
+
+    public static double getSalarProduct(Vector vector1, Vector vector2) {
+        double result = 0;
+
+        int minSize = Math.min(vector1.getSize(), vector2.getSize());
+
+        for (int i = 0; i < minSize; i++) {
+            result += vector1.getCoordinate(i) * vector1.getCoordinate(i);
+        }
+
+        return result;
     }
 }
--- Vector/src/ru/academits/pronin/vector_main/Main.java
+++ Vector/src/ru/academits/pronin/vector_main/Main.java
@@ -4,8 +4,38 @@ import ru.academits.pronin.vector.Vector;
 
 public class Main {
     public static void main(String[] args) {
-        Vector vector = new Vector(new double[]{1.0, 2.0, 3.0, 4.0});
+        Vector vector1 = new Vector(new double[]{3.0, 4.0});
 
-        System.out.println(vector);
+        System.out.println(vector1.getVectorLength());
+
+
+        Vector vector2 = new Vector(new double[]{2.3, 3.2, 5.0});
+        Vector vector3 = new Vector(new double[]{3.0, 4.0});
+
+        System.out.println(vector1.equals(vector3));
+
+        vector1.setCoordinate(5, 1);
+
+        System.out.println(vector1.getCoordinate(1));
+
+        System.out.println(vector1);
+        System.out.println(vector2);
+
+        System.out.println(vector2);
+
+        vector1.addVector(vector2);
+        System.out.println(vector1);
+
+        Vector vector4 = new Vector(new double[]{2.3, 3.2, 5.0});
+        Vector vector5 = new Vector(new double[]{3.0, 4.0});
+
+        vector4.subtractVector(vector5);
+        System.out.println(vector4);
+
+        vector1.scalarMultiplication(3);
+
+        // System.out.println(vector1);
+
+
     }
 }
