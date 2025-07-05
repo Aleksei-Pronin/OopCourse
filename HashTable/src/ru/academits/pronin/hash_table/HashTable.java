d005bf2e2a13c3ebef9d33a7307a21e3f88b056a
Pronin Aleksei <aleksei.s.pronin@gmail.com>
2025-07-05 18:00:35 +0700

    HashTable final commit

--- HashTable/src/ru/academits/pronin/hash_table/HashTable.java
+++ HashTable/src/ru/academits/pronin/hash_table/HashTable.java
@@ -2,117 +2,308 @@ package ru.academits.pronin.hash_table;
 
 import java.util.*;
 
-public class HashTable<E> implements Collection<E>  {
-    private ArrayList<E>[] table;
+public class HashTable<E> implements Collection<E> {
+    private final ArrayList<E>[] table;
     private int size;
     int modCount;
 
     public HashTable() {
         // noinspection unchecked
         table = (ArrayList<E>[]) new ArrayList[100];
-
-        for (int i = 0; i < 100; i++) {
-            table[i] = new ArrayList<>();
-        }
     }
 
     public HashTable(int size) {
         // noinspection unchecked
         table = (ArrayList<E>[]) new ArrayList[size];
-
-        for (int i = 0; i < 100; i++) {
-            table[i] = new ArrayList<>();
-        }
     }
 
-    // int index = Math.abs(o.hashCode() % array.length);
-
     @Override
-
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public boolean contains(Object o) {
-        return false;
+        if (o == null) {
+            throw new NullPointerException("Объект не должен быть null");
+        }
+
+        int index = Math.abs(o.hashCode() % table.length);
+        return table[index] != null && table[index].contains(o);
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new MyHashTableIterator();
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        Object[] objectsArray = new Object[size];
+
+        int i = 0;
+
+        for (E element : this) {
+            objectsArray[i++] = element;
+        }
+
+        return objectsArray;
     }
 
     @Override
     public <T> T[] toArray(T[] a) {
-        return null;
+        if (a == null) {
+            throw new NullPointerException("Массив не должен быть null");
+        }
+
+        if (a.length < size) {
+            // noinspection unchecked
+            a = (T[]) new Object[size];
+        } else if (a.length > size) {
+            a[size] = null;
+        }
+
+        int i = 0;
+
+        for (E element : this) {
+            // noinspection unchecked
+            a[i++] = (T) element;
+        }
+
+        return a;
     }
 
     @Override
     public boolean add(E e) {
-        return false;
+        if (e == null) {
+            throw new NullPointerException("Элемент не должен быть null");
+        }
+
+        int index = Math.abs(e.hashCode() % table.length);
+
+        if (table[index] == null) {
+            table[index] = new ArrayList<>();
+        }
+
+        table[index].add(e);
+        size++;
+        modCount++;
+        return true;
     }
 
     @Override
     public boolean remove(Object o) {
-        return false;
+        if (o == null) {
+            throw new NullPointerException("Элемент не должен быть null");
+        }
+
+        int index = Math.abs(o.hashCode() % table.length);
+
+        if (table[index] == null) {
+            return false;
+        }
+
+        boolean isRemoved = table[index].remove(o);
+
+        if (table[index].isEmpty()) {
+            table[index] = null;
+        }
+
+        if (isRemoved) {
+            size--;
+            modCount++;
+        }
+
+        return isRemoved;
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        return false;
+        if (c == null) {
+            throw new NullPointerException("Коллекция не должна быть null");
+        }
+
+        if (c.isEmpty()) {
+            return true;
+        }
+
+        if (size < c.size()) {
+            return false;
+        }
+
+        for (Object element : c) {
+            if (!contains(element)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     @Override
     public boolean addAll(Collection<? extends E> c) {
-        return false;
+        if (c == null) {
+            throw new NullPointerException("Коллекция не должна быть null");
+        }
+
+        if (c.isEmpty()) {
+            return false;
+        }
+
+        for (E element : c) {
+            add(element);
+        }
+
+        return true;
     }
 
     @Override
     public boolean removeAll(Collection<?> c) {
-        return false;
+        if (c == null) {
+            throw new NullPointerException("Коллекция не должна быть null");
+        }
+
+        if (c.isEmpty()) {
+            return false;
+        }
+
+        boolean isModified = false;
+
+        for (Object element : c) {
+            if (contains(element)) {
+                remove(element);
+                isModified = true;
+            }
+        }
+
+        return isModified;
     }
 
     @Override
     public boolean retainAll(Collection<?> c) {
-        return false;
+        if (c == null) {
+            throw new NullPointerException("Коллекция не должна быть null");
+        }
+
+        if (c.isEmpty()) {
+            clear();
+            return true;
+        }
+
+        ArrayList<E> elementsNeedRemove = new ArrayList<>();
+
+        for (E element : this) {
+            if (!c.contains(element)) {
+                elementsNeedRemove.add(element);
+            }
+        }
+
+        boolean isModified = false;
+
+        for (E element : elementsNeedRemove) {
+            remove(element);
+            isModified = true;
+        }
+
+        return isModified;
     }
 
     @Override
     public void clear() {
+        for (int i = 0; i < table.length; i++) {
+            if (table[i] != null) {
+                table[i].clear();
+                table[i] = null;
+            }
+        }
 
+        size = 0;
+        modCount++;
     }
 
-    private class MyListIterator implements Iterator<E> {
-        private int currentIndex = -1;
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        HashTable<?> hashTable = (HashTable<?>) o;
+        return size == hashTable.size && Arrays.equals(toArray(), hashTable.toArray());
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 37;
+        int hash = 1;
+        hash = prime * hash + size;
+        hash = prime * hash + Arrays.hashCode(table);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        if (size == 0) {
+            return "[]";
+        }
+
+        StringBuilder stringBuilder = new StringBuilder("[");
+
+        for (ArrayList<E> element : table) {
+            if (element != null) {
+                stringBuilder.append(element).append(", ");
+            }
+        }
+
+        stringBuilder.delete(stringBuilder.length() - 2, stringBuilder.length());
+        stringBuilder.append(']');
+        return stringBuilder.toString();
+    }
+
+    private class MyHashTableIterator implements Iterator<E> {
+        private int currentListIndex = 0;
+        private int currentElementIndexInList = -1;
+        private int returnedElementsCount = 0;
         private final int expectedModCount = modCount;
 
         @Override
         public boolean hasNext() {
-            return currentIndex + 1 < size;
+            return returnedElementsCount < size;
         }
 
         @Override
         public E next() {
-            if (expectedModCount != modCount) {
-                throw new ConcurrentModificationException("Список был изменен");
-            }
-
             if (!hasNext()) {
                 throw new NoSuchElementException("Нет следующего элемента");
             }
 
-            return items[++currentIndex];
+            if (expectedModCount != modCount) {
+                throw new ConcurrentModificationException("Список был изменен");
+            }
+
+            while (currentListIndex < table.length) {
+                if (table[currentListIndex] != null) {
+                    currentElementIndexInList++;
+
+                    if (currentElementIndexInList < table[currentListIndex].size()) {
+                        returnedElementsCount++;
+                        return table[currentListIndex].get(currentElementIndexInList);
+                    }
+                }
+
+                currentListIndex++;
+                currentElementIndexInList = -1;
+            }
+
+            throw new NoSuchElementException("Нет следующего элемента");
         }
     }
 }
--- HashTable/src/ru/academits/pronin/hash_table_main/Main.java
+++ HashTable/src/ru/academits/pronin/hash_table_main/Main.java
@@ -1,7 +1,59 @@
 package ru.academits.pronin.hash_table_main;
 
+import ru.academits.pronin.hash_table.HashTable;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        HashTable<Integer> table1 = new HashTable<>();
+        HashTable<Integer> table2 = new HashTable<>(30);
 
+        table1.add(45);
+        table1.add(4);
+        table1.add(56);
+        table1.add(34);
+        table1.add(23);
+        table1.add(756);
+        table1.add(45);
+        table1.add(98);
+        table1.add(54);
+
+        System.out.println("Размер таблицы 1 " + table1 + " - " + table1.size());
+        System.out.println("Размер таблицы 2 " + table2 + " - " + table2.size());
+        System.out.println("Таблица 1 пустая - " + table1.isEmpty());
+        System.out.println("Таблица 2 пустая - " + table2.isEmpty());
+
+
+        System.out.println("Таблица 1 содержит 4 - " + table1.contains(4));
+        System.out.println("Таблица 2 содержит 5 - " + table2.contains(5));
+
+        Object[] objectsArray = table1.toArray();
+        System.out.println("Таблица 1 в массив объектов - " + Arrays.toString(objectsArray));
+
+        System.out.println("Таблица 1 в массив new Integer[10] - " + Arrays.toString(table1.toArray(new Integer[10])));
+        System.out.println("Таблица 1 в массив new Integer[2] - " + Arrays.toString(table1.toArray(new Integer[2])));
+        System.out.println("Таблица 1 в массив new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9} - " + Arrays.toString(table1.toArray(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11})));
+
+        System.out.println("Удаляем элемент со значением 4, результат - " + table1.remove(4) + ", текущая таблица - " + table1);
+        System.out.println("Удаляем элемент со значением 11, результат - " + table1.remove((Integer) 11) + ", текущая таблица - " + table1);
+
+        table2.add(11);
+        table2.add(12);
+        table2.add(13);
+        table2.add(14);
+
+        System.out.println("Таблица 1 - " + table1);
+        System.out.println("Таблица 2 - " + table2);
+        System.out.println("Таблица 1 содержит все элементы таблицы 2 - " + table1.containsAll(table2));
+
+        System.out.println("Добавили элементы таблицы 2 к таблице 1 - " + table1.addAll(table2) + " " + table1);
+        System.out.println("Таблица 1 содержит все элементы таблицы 2 - " + table1.containsAll(table2));
+        System.out.println("Удалили все элементы таблицы 2 из таблицы 1 - " + table1.removeAll(table2) + " " + table1);
+
+        System.out.println("Добавили элементы таблицы 2 к таблице 1 - " + table1.addAll(table2) + " " + table1);
+        System.out.println("Оставили в таблице 1 только элементы таблицы 2 - " + table1.retainAll(table2) + " " + table1);
+
+        System.out.println("Таблица 1 равна таблице 2 - " + table1.equals(table2));
     }
 }
