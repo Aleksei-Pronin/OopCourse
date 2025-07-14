377fd77082148dab952d9e6fd7e3c4647d57e32c
Pronin Aleksei <aleksei.s.pronin@gmail.com>
2025-07-14 16:32:59 +0700

    Tree fixed constructor and methods

---
+++ Tree/src/ru/academits/pronin/tree/binarySearchTree.java
@@ -0,0 +1,218 @@
+package ru.academits.pronin.tree;
+
+import java.util.*;
+import java.util.function.Consumer;
+
+public class binarySearchTree<E> {
+    private final Comparator<E> comparator;
+    private TreeNode<E> root;
+    private int size;
+
+    public binarySearchTree() {
+        comparator = null;
+    }
+
+    public binarySearchTree(Comparator<E> comparator) {
+        this.comparator = comparator;
+    }
+
+    private int compare(E data1, E data2) {
+        if (comparator != null) {
+            return comparator.compare(data1, data2);
+        } else {
+            if (data1 instanceof Comparable<?> && data2 instanceof Comparable<?>) {
+                // noinspection unchecked
+                Comparable<E> comparableData1 = (Comparable<E>) data1;
+                return comparableData1.compareTo(data2);
+            } else {
+                throw new IllegalStateException("Компаратор не задан");
+            }
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean find(E data) {
+        if (root == null) {
+            return false;
+        }
+
+        TreeNode<E> currentNode = root;
+
+        while (currentNode != null) {
+            int compareResult = compare(data, currentNode.getData());
+
+            if (compareResult == 0) {
+                return true;
+            }
+
+            currentNode = (compareResult < 0) ? currentNode.getLeft() : currentNode.getRight();
+        }
+
+        return false;
+    }
+
+    public void insert(E data) {
+        if (root == null) {
+            root = new TreeNode<>(data);
+            size++;
+            return;
+        }
+
+        TreeNode<E> currentNode = root;
+
+        while (true) {
+            if (compare(data, currentNode.getData()) < 0) {
+                if (currentNode.getLeft() != null) {
+                    currentNode = currentNode.getLeft();
+                } else {
+                    currentNode.setLeft(new TreeNode<>(data));
+                    size++;
+                    return;
+                }
+            } else {
+                if (currentNode.getRight() != null) {
+                    currentNode = currentNode.getRight();
+                } else {
+                    currentNode.setRight(new TreeNode<>(data));
+                    size++;
+                    return;
+                }
+            }
+        }
+    }
+
+    public boolean remove(E data) {
+        if (root == null) {
+            return false;
+        }
+
+        TreeNode<E> currentNode = root;
+        TreeNode<E> parentNode = null;
+        boolean isLeftChild = false;
+
+        int comparedResult = compare(data, currentNode.getData());
+
+        while (comparedResult != 0) {
+            parentNode = currentNode;
+
+            if (comparedResult < 0) {
+                if (currentNode.getLeft() != null) {
+                    currentNode = currentNode.getLeft();
+                    isLeftChild = true;
+                } else {
+                    return false;
+                }
+            } else {
+                if (currentNode.getRight() != null) {
+                    currentNode = currentNode.getRight();
+                } else {
+                    return false;
+                }
+            }
+
+            comparedResult = compare(data, currentNode.getData());
+        }
+
+        if (currentNode.getLeft() == null || currentNode.getRight() == null) {
+            TreeNode<E> childNode = (currentNode.getLeft() == null) ? currentNode.getRight() : currentNode.getLeft();
+            replaceNode(parentNode, childNode, isLeftChild);
+        } else {
+            TreeNode<E> successorParentNode = currentNode;
+            TreeNode<E> successorNode = currentNode.getRight();
+
+            while (successorNode.getLeft() != null) {
+                successorParentNode = successorNode;
+                successorNode = successorNode.getLeft();
+            }
+
+            successorParentNode.setLeft(successorNode.getRight());
+            successorNode.setLeft(currentNode.getLeft());
+
+            if (currentNode.getRight() != successorNode) {
+                successorNode.setRight(currentNode.getRight());
+            }
+
+            replaceNode(parentNode, successorNode, isLeftChild);
+        }
+
+        size--;
+        return true;
+    }
+
+    public void traverseTreeInBreadth(Consumer<E> action) {
+        if (root == null) {
+            return;
+        }
+
+        Queue<TreeNode<E>> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode<E> node = queue.poll();
+            action.accept(node.getData());
+
+            if (node.getLeft() != null) {
+                queue.add(node.getLeft());
+            }
+
+            if (node.getRight() != null) {
+                queue.add(node.getRight());
+            }
+        }
+    }
+
+    public void traverseTreeInDeepRecursive(Consumer<E> action) {
+        if (root == null) {
+            return;
+        }
+
+        TreeNode<E> node = root;
+        visit(node, action);
+    }
+
+    private void visit(TreeNode<E> node, Consumer<E> action) {
+        if (node == null) {
+            return;
+        }
+
+        action.accept(node.getData());
+
+        visit(node.getLeft(), action);
+        visit(node.getRight(), action);
+    }
+
+    public void traverseTreeInDeep(Consumer<E> action) {
+        if (root == null) {
+            return;
+        }
+
+        Deque<TreeNode<E>> stack = new LinkedList<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode<E> node = stack.pop();
+            action.accept(node.getData());
+
+            if (node.getRight() != null) {
+                stack.push(node.getRight());
+            }
+
+            if (node.getLeft() != null) {
+                stack.push(node.getLeft());
+            }
+        }
+    }
+
+    private void replaceNode(TreeNode<E> parentNode, TreeNode<E> newNode, boolean isLeftChild) {
+        if (parentNode == null) {
+            root = newNode;
+        } else if (isLeftChild) {
+            parentNode.setLeft(newNode);
+        } else {
+            parentNode.setRight(newNode);
+        }
+    }
+}
--- Tree/src/ru/academits/pronin/tree_main/Main.java
+++ Tree/src/ru/academits/pronin/tree_main/Main.java
@@ -1,10 +1,10 @@
 package ru.academits.pronin.tree_main;
 
-import ru.academits.pronin.tree.Tree;
+import ru.academits.pronin.tree.binarySearchTree;
 
 public class Main {
     public static void main(String[] args) {
-        Tree<Integer> tree = new Tree<>();
+        binarySearchTree<Integer> tree = new binarySearchTree<>();
 
         tree.insert(8);
         tree.insert(3);
@@ -20,22 +20,63 @@ public class Main {
         tree.insert(25);
         tree.insert(18);
 
-        System.out.println("Обход в ширину - " + tree.traverseTreeInBreadth());
-        System.out.println("Обход в глубину - " + tree.traverseTreeInDeep());
-        System.out.println("Обход в глубину с рекурсией - " + tree.traverseTreeInDeepRecursive());
+        System.out.println("Количество элементов - " + tree.getSize());
+
+        System.out.println("Обход в ширину:");
+        tree.traverseTreeInBreadth(data -> System.out.print(data + " "));
+        System.out.println();
+        System.out.println("Обход в глубину:");
+        tree.traverseTreeInDeep(data -> System.out.print(data + " "));
+        System.out.println();
+        System.out.println("Обход в глубину с рекурсией:");
+        tree.traverseTreeInDeepRecursive(data -> System.out.print(data + " "));
+        System.out.println();
 
         System.out.println("Ищем элемент 14 - " + tree.find(14));
         System.out.println("Удаляем элемент 14 - " + tree.remove(14));
+        System.out.println("Количество элементов - " + tree.getSize());
+
         System.out.println("Ищем элемент 14 - " + tree.find(14));
         System.out.println("Удаляем элемент 14 - " + tree.remove(14));
 
-        System.out.println("Обход в ширину - " + tree.traverseTreeInBreadth());
-        System.out.println("Обход в глубину - " + tree.traverseTreeInDeep());
+        System.out.println("Обход в ширину:");
+        tree.traverseTreeInBreadth(data -> System.out.print(data + " "));
+        System.out.println();
+        System.out.println("Обход в глубину:");
+        tree.traverseTreeInDeep(data -> System.out.print(data + " "));
+        System.out.println();
 
         tree.insert(14);
         System.out.println("Добавили элемент 14");
+        System.out.println("Количество элементов - " + tree.getSize());
 
-        System.out.println("Обход в ширину - " + tree.traverseTreeInBreadth());
-        System.out.println("Обход в глубину - " + tree.traverseTreeInDeep());
+        System.out.println("Обход в ширину:");
+        tree.traverseTreeInBreadth(data -> System.out.print(data + " "));
+        System.out.println();
+        System.out.println("Обход в глубину:");
+        tree.traverseTreeInDeep(data -> System.out.print(data + " "));
+        System.out.println();
+
+        binarySearchTree<Integer> reverseTree = new binarySearchTree<>((n1, n2) -> n2.compareTo(n1));
+
+        reverseTree.insert(8);
+        reverseTree.insert(3);
+        reverseTree.insert(1);
+        reverseTree.insert(6);
+        reverseTree.insert(4);
+        reverseTree.insert(7);
+        reverseTree.insert(10);
+        reverseTree.insert(14);
+        reverseTree.insert(13);
+        reverseTree.insert(20);
+        reverseTree.insert(17);
+        reverseTree.insert(25);
+        reverseTree.insert(18);
+
+        System.out.println("Обход в ширину:");
+        reverseTree.traverseTreeInBreadth(data -> System.out.print(data + " "));
+        System.out.println();
+        System.out.println("Обход в глубину:");
+        reverseTree.traverseTreeInDeep(data -> System.out.print(data + " "));
     }
 }
